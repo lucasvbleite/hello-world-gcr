84ad67c2df74a7f39b1298a262246501f1fbf090
Lucas Leite <lucasvbleite@gmail.com>
2021-11-08 15:04:48 -0300

    pre-commit

---
+++ hello_world/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from hello_world.users.models import User
+from hello_world.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
